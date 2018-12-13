3ce5e3e4c7860dab57f6386021707451d41e5b23
pralakk16 <pralakk16@gmail.com>
2018-12-12 20:05:35 -0600

    Delete nn_reg.py

--- nn_reg.py
+++
@@ -1,7 +0,0 @@
-#import dependencies
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-#import dataset
-dataset = pd.read_csv('PlasticMarinePollutionGlobalDatasetCOPY.csv')
